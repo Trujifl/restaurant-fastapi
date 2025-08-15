7c16817cc49709544074d348027c523b58c4b7d9
Trujifl <trujicrypto@gmail.com>
2025-08-15 18:24:03 +0000

    Actualización del proyecto

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
 from database import Base, engine
 from models import order, product, table, user
-from routers import product as product_router
+from routers import product
 from routers import table as table_router
 from routers import order as order_router
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-api = FastAPI()
+app = FastAPI(
+    docs_url="/api/docs",
+    openapi_url="/api/openapi.json",
+)
 
-api.add_middleware(
+app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://3.137.206.197"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -19,16 +21,14 @@ api.add_middleware(
 
 Base.metadata.create_all(bind=engine)
 
-api.include_router(product_router.router)
-api.include_router(table_router.router)
-api.include_router(order_router.router)
+app.include_router(product.router)
+app.include_router(table_router.router)
+app.include_router(order_router.router)
 
-@api.get("/health")
+@app.get("/")
+def home():
+    return {"mensaje": "Bienvenido a la API de comandas del restaurante"}
+
+@app.get("/api/health")
 def health():
     return {"ok": True}
-
-@api.get("/")
-def api_root():
-    return {"message": "Restaurant API is running"}
-
-app.mount("/api", api)
